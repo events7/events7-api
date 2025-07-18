2b8f8a0f1164317a111f651dbc6251441cb1ff57
orangeGoran <mail.goran96@gmail.com>
2025-07-18 19:41:02 +0200

    Adds nvmrc, adds .env.example, adds docker compose for database, adds openapi, adds typeorm, adds versioning, adds events/* enpoints,

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({}),
+    EventsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: parseInt(process.env.POSTGRES_PORT ?? '5432'),
+      username: process.env.POSTGRES_USER ?? 'root',
+      password: process.env.POSTGRES_PASSWORD ?? 'root',
+      database: process.env.POSTGRES_DB ?? 'test',
+      entities: [],
+      // TODO: this could be potential risk of data loss
+      // TODO: keeping it simple for now
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set up OpenAPI Swagger
+  const config = new DocumentBuilder()
+    .setTitle(process.env.API_TITLE ?? 'Events7 API')
+    .setDescription(
+      process.env.API_DESCRIPTION ?? 'The Events7 API description',
+    )
+    .setVersion(process.env.API_VERSION ?? '2.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
+  // Enable versioning into URI
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  // Start listening
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// NOTE: Disable `no-floating-promises` rule - we
+// NOTE: want it to fail on unhandled rejections
+// NOTE: from `bootstrap`
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
